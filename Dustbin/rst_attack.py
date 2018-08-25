0dd97d201c5608c9316fe78a27d0a68e52b8e2e4
hsxhr-10 <huangzongzhuan@gmail.com>
2018-08-25 13:05:32 +0800

    Update rst_attack.py

--- Dustbin/rst_attack.py
+++ Dustbin/rst_attack.py
@@ -4,8 +4,10 @@ import socket
 HOST = '114.55.102.246'                 # Symbolic name meaning all available interfaces
 PORT = 6666           # Arbitrary non-privileged port
 
-# HOST = '101.37.187.164'                 # Symbolic name meaning all available interfaces
-# PORT = 443           # Arbitrary non-privileged port
+# Symbolic name meaning all available interfaces
+# HOST = '101.37.187.164'
+# Arbitrary non-privileged port
+# PORT = 443           
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
 s.connect((HOST, PORT))
